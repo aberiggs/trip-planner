46efeec7a2a97a776021460a23364a75155149a3
chi-wei-lien <wilsonwilson3124@gmail.com>
2024-06-12 21:06:46 -0400

    add util function get_secret

---
+++ back-end/functions/util/get_secret.py
@@ -0,0 +1,26 @@
+import boto3
+from botocore.exceptions import ClientError
+import json
+
+def get_secret(secret_key):
+    secret_name = "mongo-db-connection-secret"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+        get_secret_value_response = json.loads(get_secret_value_response['SecretString'])
+    except ClientError as e:
+        raise e
+    
+    if secret_key not in get_secret_value_response:
+        return "secret_key doesn't exist"
+
+    return get_secret_value_response[secret_key]
