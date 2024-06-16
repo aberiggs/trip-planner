223a66a48a86fe7d30e96b628489636d90aeb207
chi-wei-lien <lien2@purdue.edu>
2024-06-16 18:21:23 -0400

    set up basic login in frontend

--- front-end/app/index.tsx
+++ front-end/app/index.tsx
@@ -1,39 +1,40 @@
 import { View, Text, Button } from "react-native";
 import { StyleSheet } from "react-native";
 import * as WebBrowser from "expo-web-browser";
-import * as Google from "expo-auth-session/providers/google"
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Google from "expo-auth-session/providers/google";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
+import { saveItem, getItem, removeItem } from "@/utils/async-store";
+import { getSecret, saveSecret, removeSecret } from "@/utils/secure-store";
 
 WebBrowser.maybeCompleteAuthSession();
 
 const styles = StyleSheet.create({
-  centered: { 
-    flex: 1, 
-    justifyContent: "center", 
-    alignItems: "center", 
-  }, 
-  title: { 
-    fontSize: 18, 
-    marginVertical: 2, 
-  }, 
-  subtitle: { 
-    fontSize: 14, 
-  }
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 18,
+    marginVertical: 2,
+  },
+  subtitle: {
+    fontSize: 14,
+  },
 });
 
 interface User {
-  "user_id": string
-  "email": string
-  "picture": string
-  "name": string
+  user_id: string;
+  email: string;
+  picture: string;
+  name: string;
 }
 
 interface JwtHeader {
-  alg: string,
-  exp: number, 
-  typ: string
+  alg: string;
+  exp: number;
+  typ: string;
 }
 
 export default function Index() {
@@ -47,45 +48,75 @@ export default function Index() {
 
   useEffect(() => {
     handleGoogleSignIn();
-  }, [fullResult])
+  }, [fullResult]);
 
-  const handleGoogleSignIn = async() => {
-    const user = await AsyncStorage.getItem("user");
-    const jwtToken = await AsyncStorage.getItem("jwt");
+  const handleGoogleSignIn = async () => {
+    const user = await getItem("user");
+    const jwtToken = await getSecret("jwt");
 
     if (!user || !jwtToken) {
       if (fullResult?.type === "success") {
-        const response = await fetch("http://127.0.0.1:3000/auth", {
-          method: "POST",
-          body: JSON.stringify({
-            "idToken": fullResult.params.id_token
-          }),
-          headers: {
-            "Content-Type": "application/json"
+        const response = await fetch(
+          `${process.env.EXPO_PUBLIC_BACKEND_URL}/auth`,
+          {
+            method: "POST",
+            body: JSON.stringify({
+              idToken: fullResult.params.id_token,
+              client_type: "ios",
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
           }
-        })
+        );
         const data = await response.json();
-        const jwtToken = data["jwt"]
+        const jwtToken = data["jwt"];
         const user = jwtDecode<User>(jwtToken);
         const header = jwtDecode<JwtHeader>(jwtToken, { header: true });
 
-        // TODO: check jwt token has not expired
-        await AsyncStorage.setItem("user", JSON.stringify(user));
-        await AsyncStorage.setItem("jwt", jwtToken);
+        if (Number(header["exp"]) < new Date().getTime()) {
+          // jwt token has expired, prompt user to log in again
+        }
+        await saveItem("user", JSON.stringify(user));
+        await saveSecret("jwt", jwtToken);
         setUserInfo(user);
       }
+    } else {
+      setUserInfo(JSON.parse(user));
     }
-    else {
-      setUserInfo(JSON.parse(user))
-    }
-  }
+  };
+
+  const deleteLocalStorage = () => {
+    removeItem("user");
+    removeSecret("jwt");
+  };
+
+  const testLogin = async () => {
+    const jwtToken = await getSecret("jwt");
+    const response = await fetch(
+      `${process.env.EXPO_PUBLIC_BACKEND_URL}/test_auth`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <View style={styles.centered}>
       <Text>{JSON.stringify(userInfo, null, 2)}</Text>
       <Text>Login</Text>
       <Button title="sign in with google" onPress={() => promptAsync()} />
-      <Button title="delete local storage" onPress={() => AsyncStorage.removeItem("user")}></Button>
+      <Button
+        title="delete local storage"
+        onPress={() => deleteLocalStorage()}
+      />
+      <Button title="test login" onPress={() => testLogin()} />
     </View>
   );
 }
