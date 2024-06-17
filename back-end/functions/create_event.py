46c5e39aabd6554200dd26e43442067c57c4845e
Alan Hou <alanhou911222@gmail.com>
2024-06-17 20:53:58 +0800

    create event backend function

--- back-end/functions/create_event.py
+++ back-end/functions/create_event.py
@@ -1,11 +1,28 @@
 import json
-from lib.get_secret import get_secret
+from util.get_secret import get_secret
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+mongo_db_connect_url = get_secret("mongo_db_connect_url")
+client = MongoClient(mongo_db_connect_url, server_api=ServerApi("1"))
 
 def lambda_handler(event, context):
-    mongo_db_user = get_secret('mongo_db_user')
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": mongo_db_user,
-        }),
-    }
+    try:
+        # insert a test event document to MongoDB, Todo: error handling
+        insert_result = client["tripPlanner"]["testEvents"].insert_one(json.loads(event["body"]))
+
+        return {
+            "statusCode": 201,
+            "body": json.dumps({
+                "inserted_id": str(insert_result.inserted_id),
+            }),
+        }
+    
+    except Exception as e:
+        return {
+            "statusCode": 503,
+            "body": json.dumps({
+                "message": str(e),
+            }),
+        }
+    
